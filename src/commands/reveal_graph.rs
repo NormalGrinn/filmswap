dbb7b0f8f7dbb6d4e359660cc36678009c58199a
coffeee <Coffeee@drinkable.com>
2026-09-11 17:23:43 -0400

    removed concurrency as graphviz wouldnt accept a stream, likely anyway, also made is so i didnt touch a bunch of stuff

--- src/commands/reveal_graph.rs
+++ src/commands/reveal_graph.rs
@@ -1,23 +1,26 @@
 use crate::{
-    Context, Error, database, types::Phase, utilities::ensure_host_role,types::GraphLayout
+    Context, Error, database, types::GraphLayout, types::Phase, utilities::ensure_host_role,
 };
+use petgraph::Graph;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::NodeIndex;
 use poise::CreateReply;
 use rusqlite::Result;
-use serenity::all::{ CreateMessage, CreateAttachment };
-use petgraph::Graph;
-use petgraph::graph::NodeIndex;
-use petgraph::dot::{Dot, Config};
+use serenity::all::{CreateAttachment, CreateMessage};
+use std::process::Stdio;
+use tokio::io::AsyncWriteExt;
 use tokio::process::Command as TokioCommand;
 #[poise::command(prefix_command, slash_command)]
 pub async fn reveal_graph(
     ctx: Context<'_>,
-    #[description = "Graph layout"]
-    mut layout: GraphLayout,
+    #[description = "Graph layout"] mut layout: GraphLayout,
 ) -> Result<(), Error> {
     if !ensure_host_role(&ctx, ctx.author()).await? {
         return Ok(());
     }
-    if !crate::utilities::ensure_correct_phase(&ctx, vec![Phase::Swap, Phase::Watch]).await? {return Ok(())}
+    if !crate::utilities::ensure_correct_phase(&ctx, vec![Phase::Swap, Phase::Watch]).await? {
+        return Ok(());
+    }
     let users = match database::get_matching_order() {
         Ok(users) => users,
         Err(e) => {
@@ -33,39 +36,6 @@ pub async fn reveal_graph(
             return Ok(());
         }
     };
-
-    let mut graph = Graph::<&str, &str>::new();
-    let mut user_nodes: Vec<NodeIndex> = Vec::new();
-    let mut edges: Vec<(NodeIndex, NodeIndex)> = Vec::new();
-    // create node - store in user_nodes
-    users.iter()
-        .for_each(|user| {
-            user_nodes.push(graph.add_node(user.1.as_str()));
-        });
-    //create edges 
-    for i in 0..user_nodes.len() {
-        edges.push((user_nodes[i], user_nodes[(i + 1) % user_nodes.len()]));
-    }
-    // push edges to graph
-    graph.extend_with_edges(&edges);
-
-    //create dot file
-    {
-        let dot_output = format!("{}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-        
-        if let Err(e) = std::fs::write("graph.dot", &dot_output) {
-            eprintln!("Error writing graph to file: {}", e);
-            ctx.send(
-                CreateReply::default()
-                    .content("There was a problem with generating the graph dot file")
-                    .ephemeral(true),
-            )
-            .await?;
-            tokio::fs::remove_file("graph.dot").await?;
-            return Ok(());
-        }   
-    }
- 
     layout = match layout {
         GraphLayout::Default => GraphLayout::Circo,
         GraphLayout::Random => {
@@ -77,51 +47,89 @@ pub async fn reveal_graph(
                 GraphLayout::Twopi,
                 GraphLayout::Osage,
                 GraphLayout::Patchwork,
-                ];
-             layouts[rand::random_range(0..layouts.len())]
+            ];
+            layouts[rand::random_range(0..layouts.len())]
         }
-        _ => { layout }
+        _ => layout,
     };
-    let message = format!("heres the graph :happy: ({:?})", layout);
-    let graph_creation_status = TokioCommand::new(format!("{:?}", layout).to_lowercase())
-        .args(["-Tpng", "graph.dot", "-o", "graph.png", "-Nshape=none"])
-        .status()
-        .await;
-
-    if let Err(e) = graph_creation_status {
-        eprintln!("Command failed: {}", e);
-        ctx.send(
-            CreateReply::default()
-                .content("Command failed, feature may not be available")
-                .ephemeral(true),
-        )
-        .await?;
-        tokio::fs::remove_file("graph.dot").await?;
-        tokio::fs::remove_file("graph.png").await?;
-        return Ok(());
+    let mut graph = Graph::<&str, &str>::new();
+    let mut user_nodes: Vec<NodeIndex> = Vec::new();
+    let mut edges: Vec<(NodeIndex, NodeIndex)> = Vec::new();
+    // create node on graph then store in user_nodes
+    users.iter().for_each(|user| {
+        user_nodes.push(graph.add_node(user.1.as_str()));
+    });
+    //create edges
+    for i in 0..user_nodes.len() {
+        edges.push((user_nodes[i], user_nodes[(i + 1) % user_nodes.len()]));
     }
+    // push edges to graph
+    graph.extend_with_edges(&edges);
+    let dot_output = format!("{}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+    let message = format!("heres the graph :happy: ({:?})", layout);
 
-    let attachment = match CreateAttachment::path("graph.png").await {
-        Ok(attachment) => attachment,
-        Err(e) => {
-            eprintln!("Error creating attachment: {}", e);
+    let mut graphviz_process = match TokioCommand::new(format!("{:?}", layout).to_lowercase())
+        .arg("-Tpng")
+        .arg("-Nshape=none")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()
+    {
+        Ok(process) => process,
+        Err(_) => {
             ctx.send(
                 CreateReply::default()
-                    .content("Couldnt create graph attachment")
+                    .content("Command failed, feature may not be available")
                     .ephemeral(true),
             )
             .await?;
-            tokio::fs::remove_file("graph.dot").await?;
-            tokio::fs::remove_file("graph.png").await?;
             return Ok(());
         }
     };
-    match ctx.author().direct_message(
-        ctx.http(),
-        CreateMessage::new()
-            .content(message)
-            .add_file(attachment),
-    ).await {
+    let mut graphviz_stdin = graphviz_process.stdin.take().unwrap();
+    if let Err(e) = graphviz_stdin.write_all(dot_output.as_bytes()).await {
+        eprintln!("Error: {:?}", e);
+        ctx.send(
+            CreateReply::default()
+                .content("Failed to execute command")
+                .ephemeral(true),
+        )
+        .await?;
+        return Ok(());
+    };
+    drop(graphviz_stdin);
+    let graph_data = match graphviz_process.wait_with_output().await {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            ctx.send(
+                CreateReply::default()
+                    .content("Command failed")
+                    .ephemeral(true),
+            )
+            .await?;
+            return Ok(());
+        }
+    };
+    if !graph_data.status.success() {
+        ctx.send(
+            CreateReply::default()
+                .content("Command failed")
+                .ephemeral(true),
+        )
+        .await?;
+        return Ok(());
+    }
+    let attachment = CreateAttachment::bytes(graph_data.stdout, "graph.png");
+    match ctx
+        .author()
+        .direct_message(
+            ctx.http(),
+            CreateMessage::new().content(message).add_file(attachment),
+        )
+        .await
+    {
         Ok(_) => {
             ctx.send(
                 CreateReply::default()
@@ -141,7 +149,5 @@ pub async fn reveal_graph(
             .await?;
         }
     }
-    tokio::fs::remove_file("graph.png").await?;
-    tokio::fs::remove_file("graph.dot").await?;
     Ok(())
 }
